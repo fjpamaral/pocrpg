d3c50e21b8cf20d7925ec9de206506faa30b75eb
Felipe Amaral <felipe.jpa94@gmail.com>
2020-05-06 11:05:16 -0300

    projRename

--- ios/POCRPGTests/POCRPGTests.m
+++ ios/POCRPGTests/POCRPGTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface POCRPGTests : XCTestCase
+@interface pocrpgTests : XCTestCase
 
 @end
 
-@implementation POCRPGTests
+@implementation pocrpgTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
